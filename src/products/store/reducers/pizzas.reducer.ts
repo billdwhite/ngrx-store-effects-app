32b610632b4ea38459d2d5849f6261e87265ae0d
Bill White <bwhite@planview.com>
2018-06-11 12:07:23 -0500

    up to (not including) effects and entities

---
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -0,0 +1,105 @@
+import * as fromPizzas from '../actions/pizzas.action';
+import {Pizza} from '../../models/pizza.model';
+
+
+export interface PizzaState {
+    data: Pizza[],
+    loaded: boolean,
+    loading: boolean
+}
+
+
+
+export const initialState: PizzaState = {
+    data: [
+        {
+            "name": "Seaside Surfin'",
+            "toppings": [
+                {
+                    "id": 6,
+                    "name": "mushroom"
+                },
+                {
+                    "id": 7,
+                    "name": "olive"
+                },
+                {
+                    "id": 2,
+                    "name": "bacon"
+                },
+                {
+                    "id": 3,
+                    "name": "basil"
+                },
+                {
+                    "id": 1,
+                    "name": "anchovy"
+                },
+                {
+                    "id": 8,
+                    "name": "onion"
+                },
+                {
+                    "id": 11,
+                    "name": "sweetcorn"
+                },
+                {
+                    "id": 9,
+                    "name": "pepper"
+                },
+                {
+                    "id": 5,
+                    "name": "mozzarella"
+                }
+            ],
+            "id": 2
+        }
+    ],
+    loaded: false,
+    loading: false
+};
+
+
+
+export function reducer(
+    state:PizzaState=initialState, 
+    action: fromPizzas.PizzasAction
+): PizzaState   {
+
+    switch(action.type) {
+        case fromPizzas.LOAD_PIZZAS: {
+            return {
+                ...state, 
+                loading: true
+            };
+        }
+        case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+            return {
+                ...state,
+                loading: false, 
+                loaded: true
+            };
+        }
+
+    }
+
+    return state;
+}
+
+
+
+export const getPizzasLoading = (state: PizzaState) => {
+    return state.loading;
+}
+
+
+
+export const getPizzasLoaded = (state: PizzaState) => {
+    return state.loaded;
+}
+
+
+
+export const getPizzas = (state: PizzaState) => {
+    return state.data;
+}
