417d323af8682090418ed97166082c2e83c27834
Bill White <bwhite@planview.com>
2018-06-11 16:10:06 -0500

    Up to (but not including) selector composition

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -13,7 +13,7 @@ import { Topping } from "../../models/topping.model";
       class="product-item">
       <pizza-form
         [pizza]="pizza$ | async"
-        [toppings]="toppings"
+        [toppings]="toppings$ | async"
         (selected)="onSelect($event)"
         (create)="onCreate($event)"
         (update)="onUpdate($event)"
@@ -28,11 +28,12 @@ import { Topping } from "../../models/topping.model";
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
   visualise: Pizza;
-  toppings: Topping[];
+  toppings$: Observable<Topping[]>;
 
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
+    this.store.dispatch(new fromStore.LoadToppings());
     this.pizza$ = this.store.select(fromStore.getSelectedPizza);
   }
 
---
+++ src/products/store/reducers/toppings.reducer.ts
@@ -0,0 +1,68 @@
+import  * as fromToppings from '../actions/toppings.action';
+import { Topping } from '../../models/topping.model';
+
+export interface ToppingsState {
+    entities: {
+        [id: number]: Topping
+    },
+    loaded: boolean;
+    loading: boolean;
+}
+
+
+export const initialState: ToppingsState = {
+    entities: {},
+    loaded: false,
+    loading: false
+}
+
+
+export function reducer(
+    state=initialState,
+    action: fromToppings.ToppingsAction
+): ToppingsState {
+
+    switch(action.type) {
+        case fromToppings.LOAD_TOPPINGS: {
+            return {
+                ...state,
+                loading: true
+            }
+        }
+
+        case fromToppings.LOAD_TOPPINGS_SUCCESS: {
+            const toppings: Topping[] = action.payload;
+
+            const entities = toppings.reduce(
+                (entities: { [id: number]: Topping }, topping: Topping) => { 
+                    return {
+                        ...entities,
+                        [topping.id]: topping // es6 syntax; dynamically creates properties by id
+                    };
+                }, 
+                {
+                    ...state.entities
+                }
+            );
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                entities: entities
+            }
+        }
+
+        case fromToppings.LOAD_TOPPINGS_FAIL: {
+            return {
+                ...state,
+                loaded: false,
+                loading: false
+            }
+        }
+    }
+    return state;
+}
+
+export const getToppingEntities = (state: ToppingsState) => state.entities;
+export const getToppingLoaded   = (state: ToppingsState) => state.loaded;
+export const getToppingLoading  = (state: ToppingsState) => state.loading;
